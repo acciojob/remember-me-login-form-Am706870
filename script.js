01a9980ede9551f284e33988b7c2c70f2170f039
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-04-04 00:01:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+function checkExistingUser() {
+    const savedUsername = localStorage.getItem('username');
+    const savedPassword = localStorage.getItem('password');
+
+    if (savedUsername && savedPassword) {
+        document.getElementById('existing').style.display = 'block';
+    }
+}
+document.getElementById("form").addEventListener('submit',function(event){
+	event.preventDefault()
+
+	const username = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
+	const checkvalue = document.getElementById("checkbox").checked;
+
+	alert(`Logged in as ${username}`);
+
+	if (checkvalue) {
+		localStorage.setItem('username', username);
+		localStorage.setItem('password', password);
+	}else {
+		localStorage.removeItem('username');
+		localStorage.removeItem('password');
+	}
+	window.location.reload();
+});
+document.getElementById('existing').addEventListener('click',function(){
+	const starage = localStorage.getItem('username');
+	alert(`Logged in as ${starage}`);
+});
+window.onload = function(){
+
+	checkExistingUser()
+}
+
